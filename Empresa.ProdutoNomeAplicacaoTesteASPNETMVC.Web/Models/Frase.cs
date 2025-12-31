dc70829811d2e4426436ec3e1a658a9d63fd0695
Edi Carlos <edi.carlos@divtech.com.br>
2025-12-31 15:15:20 -0300

    ComponentModel | Validação Client Side | Data Notations

--- Empresa.ProdutoNomeAplicacaoTesteASPNETMVC.Web/Models/Frase.cs
+++ Empresa.ProdutoNomeAplicacaoTesteASPNETMVC.Web/Models/Frase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,14 @@ namespace Empresa.ProdutoNomeAplicacaoTesteASPNETMVC.Web.Models
 {
     public class Frase
     {
+        [Required(ErrorMessage = "A Frase deve ser preenchida")]
+        [DisplayName("Descrição da Frase: ")]
         public string Descricao { get; set; }
 
+        [Required(ErrorMessage = "A categoria deve ser preenchida")]
+        [DisplayName("Categoria da Frase: ")]
         public string Categoria { get; set; }
-
+        [DisplayName("Likes da Frase: ")]
         public int Likes { get; set; }
     }
 }
--- Empresa.ProdutoNomeAplicacaoTesteASPNETMVC.Web/Models/Pessoa.cs
+++ Empresa.ProdutoNomeAplicacaoTesteASPNETMVC.Web/Models/Pessoa.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,10 +9,24 @@ namespace Empresa.ProdutoNomeAplicacaoTesteASPNETMVC.Web.Models
 {
     public class Pessoa
     {
+        [Required(ErrorMessage = "O nome é obrigatório")]
+        [DisplayName("Nome da Pessoa: ")]
+        [MaxLength(50, ErrorMessage = "O nome pode ter no máximo 50 caracteres")]
+        [MinLength(5, ErrorMessage = "O nome deve conter no mínimo 5 caracteres")]
         public string Nome { get; set; }
 
+        [DisplayName("Idade da pessoa: ")]
         public int Idade { get; set; }
 
+        [Required(ErrorMessage = "O endereço da pessoa é obrigatório")]
+        [DisplayName("Endereço da Pessoa: ")]
+        [MaxLength(100, ErrorMessage = "O endereço pode ter no máximo 100 caracteres")]
         public string Endereco { get; set; }
+
+        [Required(ErrorMessage = "O e-mail da pessoa é obrigatório")]
+        [DisplayName("E-mail da pessoa: ")]
+        [MaxLength(20, ErrorMessage = "O e-mail pode ter no máximo 20 caracteres")]
+        [DataType(DataType.EmailAddress)]
+        public string Email { get; set; }
     }
 }
